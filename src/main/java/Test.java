cea271228cb0a9a01522c2bf485bb4b64dceda9d
Vishal Gaikwad <vishalgofficial@gmail.com>
2020-10-23 13:44:11 +0530

    DeadLock

---
+++ src/main/java/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+    public static void main(String[] args) {
+        // creating one object 
+        Shared s1 = new Shared();
+
+        // creating second object 
+        Shared s2 = new Shared();
+
+        // creating first thread and starting it 
+        Thread1 t1 = new Thread1(s1, s2);
+        t1.start();
+
+        // creating second thread and starting it 
+        Thread2 t2 = new Thread2(s1, s2);
+        t2.start();
+
+        // sleeping main thread 
+        Util.sleep(2000);
+    }
+} 
